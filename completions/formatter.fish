6fcddb4bc866313567f7187e3414af9ec31abaa6
ant1k9 <ant1k9@protonmail.com>
2021-11-17 15:12:45 +0700

    Add completions for fish shell

---
+++ completions/formatter.fish
@@ -0,0 +1,27 @@
+complete -f -c formatter \
+    -l help \
+    -d "show usage for command"
+
+complete -f -c formatter \
+    -l prefix \
+    -d "prefix for every line"
+
+complete -f -c formatter \
+    -l suffix \
+    -d "suffix for every line"
+
+complete -f -c formatter \
+    -l header \
+    -d "header for the whole output"
+
+complete -f -c formatter \
+    -l footer \
+    -d "footer for the whole output"
+
+complete -f -c formatter \
+    -l line-breaker \
+    -d "line breaker between output lines"
+
+complete -f -c formatter \
+    -l width \
+    -d "fix width for every line"
