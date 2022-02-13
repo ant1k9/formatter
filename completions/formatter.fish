7dbb4bba5291ecbdbd7e29b03e46523513910190
ant1k9 <ant1k9@protonmail.com>
2022-02-13 15:57:15 +0300

    fix: update completions for fish shell

--- completions/formatter.fish
+++ completions/formatter.fish
@@ -4,24 +4,30 @@ complete -f -c formatter \
 
 complete -f -c formatter \
     -l prefix \
+    -n "not __fish_seen_subcommand_from --prefix" \
     -d "prefix for every line"
 
 complete -f -c formatter \
     -l suffix \
+    -n "not __fish_seen_subcommand_from --suffix" \
     -d "suffix for every line"
 
 complete -f -c formatter \
     -l header \
+    -n "not __fish_seen_subcommand_from --header" \
     -d "header for the whole output"
 
 complete -f -c formatter \
     -l footer \
+    -n "not __fish_seen_subcommand_from --footer" \
     -d "footer for the whole output"
 
 complete -f -c formatter \
     -l line-breaker \
+    -n "not __fish_seen_subcommand_from --line-breaker" \
     -d "line breaker between output lines"
 
 complete -f -c formatter \
     -l width \
+    -n "not __fish_seen_subcommand_from --width" \
     -d "fix width for every line"
